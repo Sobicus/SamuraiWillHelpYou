b3900f491378a96307b4c688b705ac8bb273612c
Sobicus <83043123+Sobicus@users.noreply.github.com>
2023-12-05 03:04:05 +0100

    samurai mentor #30-31 finished

--- samurai-way_30-31/src/components/Profile/MyPosts/MyPosts.tsx
+++ samurai-way_30-31/src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,24 +1,30 @@
 import React from "react";
-import { MyPostsMessagesDataType } from "../../../redux/state";
+import {MyPostsMessagesDataType} from "../../../redux/state";
 import style from './MyPosts.module.css'
 import {Post} from "./Post/Post";
 
 export const MyPosts = (props: MyPostsType) => {
     const {myPostsMessagesData} = props
+    let newPostElement=React.createRef<HTMLTextAreaElement>()
+    const addPost = ()=>{
+        let text = newPostElement.current?.value
+        alert(text)
+    }
     return (
         <div className={style.content}>
             <div>
                 <h3>My Posts</h3>
                 <div>
                     <div>
-                        <textarea></textarea>
+                        <textarea ref={newPostElement}></textarea>
                     </div>
                     <div>
-                        <button>Add post</button>
+                        <button onClick={addPost}>Add post</button>
                     </div>
                 </div>
                 <div className={style.posts}>
-                    {props.myPostsMessagesData.map((myPostsMessagesData) => <Post message={myPostsMessagesData.message} likeCount={myPostsMessagesData.likeCount}/>)}
+                    {props.myPostsMessagesData.map((myPostsMessagesData) => <Post message={myPostsMessagesData.message}
+                                                                                  likeCount={myPostsMessagesData.likeCount}/>)}
                 </div>
             </div>
         </div>
