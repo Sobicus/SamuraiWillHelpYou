0e51618866dc3b57de60274a78a6180f5e267285
Sobicus <83043123+Sobicus@users.noreply.github.com>
2024-01-02 02:11:46 +0100

    social-network #48 start

---
+++ samurai-way_48/src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import {Navbar} from "./components/Navbar/Navbar";
+import {Profile} from "./components/Profile/Profile";
+import {Header} from "./components/Header/Header";
+import {Route} from "react-router-dom";
+import {News} from "./components/News/News";
+import {Music} from "./components/Music/Music";
+import {Settings} from './components/Settings/Settings';
+import {DialogsContainer} from "./components/Dialogs/DialogsContainer";
+
+
+export const App = () => {
+    return (
+        <div className='app-wrapper'>
+            <Header/>
+            <Navbar/>
+            <div className='app-wrapper-content'>
+                <Route path='/profile' render={() => <Profile/>}/>
+                <Route /*exact*/ path='/dialogs'
+                                 render={() => <DialogsContainer/>}/>
+                <Route path='/news' render={() => <News/>}/>
+                <Route path='/music' render={() => <Music/>}/>
+                <Route path='/settings' render={() => <Settings/>}/>
+            </div>
+        </div>
+    );
+}
---
+++ samurai-way_48/src/redux/dialog-reducer.ts
@@ -0,0 +1,58 @@
+import {
+    ActionsType,
+    DialogsPageType,
+} from "./store";
+
+export const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY'
+export const SEND_MESSAGE = 'SEND-MESSAGE'
+
+let initialState: DialogsPageType = {
+    dialogsData: [
+        {id: 1, name: 'Viktoriia'},
+        {id: 2, name: 'Mark'},
+        {id: 3, name: 'Alina'},
+        {id: 4, name: 'Maks'},
+        {id: 5, name: 'Lubov'},
+        {id: 6, name: 'Anatoliy'},
+    ],
+    messagesData: [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'How is your going?'},
+        {id: 3, message: 'Good, and you?'},
+    ],
+    newMessageBody: '',
+}
+
+export const dialogReducer = (state: DialogsPageType = initialState, action: ActionsType) => {
+    switch (action.type) {
+        case UPDATE_NEW_MESSAGE_BODY: {
+            // state.newMessageBody = action.newMessageText
+            // return state
+            return {...state, newMessageBody: action.newMessageText}
+        }
+        case SEND_MESSAGE: {
+            // const body = state.newMessageBody
+            // state.newMessageBody = ''
+            // state.messagesData.push({id: 4, message: body})
+            // return state
+            return {...state, messagesData: [...state.messagesData, {id: 4, message: state.newMessageBody}], newMessageBody: ''}
+        }
+        default:
+            return state
+    }
+}
+export const sendMessageActionCreator = (): SendMessageActionCreatorType => {
+    return {type: SEND_MESSAGE}
+}
+export const updateNewMessageBodyActionCreator = (newMessageText: string): UpdateNewMessageBodyActionCreatorType => {
+    return {type: UPDATE_NEW_MESSAGE_BODY, newMessageText}
+}
+//export type ActionsDialogsType = SendMessageActionCreatorType | UpdateNewMessageBodyActionCreatorType
+// type sendMessageActionCreatorType = ReturnType<typeof sendMessageActionCreator>
+// type updateNewMessageBodyActionCreatorType = ReturnType<typeof updateNewMessageBodyActionCreator>
+export type SendMessageActionCreatorType = {
+    type: 'SEND-MESSAGE'
+}
+export type UpdateNewMessageBodyActionCreatorType = {
+    type: 'UPDATE-NEW-MESSAGE-BODY', newMessageText: string
+}
