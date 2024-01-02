67687cc7186a7bb016fa6a678396e1d43130497e
Sobicus <83043123+Sobicus@users.noreply.github.com>
2024-01-02 03:18:35 +0100

    social-network #48 finish

--- samurai-way_48/src/components/Dialogs/DialogsContainer.tsx
+++ samurai-way_48/src/components/Dialogs/DialogsContainer.tsx
@@ -4,18 +4,7 @@ import {Dialogs} from "./Dialogs";
 import {Dispatch} from "redux";
 import {connect} from "react-redux";
 
-/*export const DialogsContainer = (props: DialogsType) => {
-    let state = props.store.getState().dialogsPage
-    let onSendMessageClick = () => {
-        props.store.dispatch(sendMessageActionCreator())
-    }
-    let onNewMessageChange = (newMessageBody: string) => {
-        props.store.dispatch(updateNewMessageBodyActionCreator(newMessageBody))
-    }
-    return (
-        <Dialogs updateNewMessageBody={onNewMessageChange} sendMessage={onSendMessageClick} dialogsPage={state}/>
-    )
-}*/
+
 let mapStateToProps= (state: StateType) => {
     return{
         dialogsPage:state.dialogsPage
@@ -29,6 +18,3 @@ let mapDispatchToProps= (dispatch:Dispatch) => {
 }
 
 export const DialogsContainer = connect(mapStateToProps,mapDispatchToProps)(Dialogs)
-/*type DialogsType = {
-    store: StoreType
-}*/
