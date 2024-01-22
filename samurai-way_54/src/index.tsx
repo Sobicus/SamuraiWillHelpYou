f144005408d226e2dab1d2812af95d31441b556a
Sobicus <83043123+Sobicus@users.noreply.github.com>
2024-01-22 17:58:07 +0100

    socilNetwork #54 start

---
+++ samurai-way_54/src/index.tsx
@@ -0,0 +1,20 @@
+import './index.css';
+import ReactDOM from "react-dom";
+import {App} from "./App";
+import React from "react";
+import {BrowserRouter} from 'react-router-dom';
+import {store} from './redux/redux-store';
+import {Provider} from "react-redux";
+
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </BrowserRouter>,
+    document.getElementById('root')
+)
+
+
+
