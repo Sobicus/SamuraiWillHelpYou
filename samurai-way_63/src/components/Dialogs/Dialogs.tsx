8ec31a8eea3f90613c18c1499d87ae65693800ae
Sobicus <83043123+Sobicus@users.noreply.github.com>
2024-01-30 10:27:08 +0100

    SN #63 start

---
+++ samurai-way_63/src/components/Dialogs/Dialogs.tsx
@@ -0,0 +1,50 @@
+import style from './Dialogs.module.css'
+import {DialogItem} from "./DialogItem/DialogsItem";
+import {Message} from "./Message/Message";
+import {
+    DialogsPageType
+} from "../../redux/store";
+import {ChangeEvent} from 'react';
+
+export const Dialogs = (props: DialogsType) => {
+    let state = props.dialogsPage
+    let dialogsElements = state.dialogsData.map((dialog) => <DialogItem name={dialog.name} id={dialog.id}/>)
+    let messagesElements = state.messagesData.map((message) => <Message message={message.message}
+                                                                        id={message.id}/>)
+    let onSendMessageClick = () => {
+        props.sendMessageAC()
+    }
+    let newMessageBody = state.newMessageBody
+    let onNewMessageChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        let newMessageBody = e.target.value
+        //props.store.dispatch(updateNewMessageBodyActionCreator(newMessageBody))
+        props.updateNewMessageBodyAC(newMessageBody)
+    }
+    return (
+        <div className={style.dialogs}>
+            <div className={style.dialogsItems}>
+                {dialogsElements}
+            </div>
+            <div className={style.messages}>
+                <div>{messagesElements}</div>
+                <div>
+                    <div>
+                        <textarea placeholder='Enter your messages'
+                                  value={newMessageBody}
+                                  onChange={onNewMessageChange}
+                        ></textarea>
+                    </div>
+                    <div>
+                        <button onClick={onSendMessageClick}>Send</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+type DialogsType = {
+    /*store: StoreType*/
+    updateNewMessageBodyAC: (newMessageBody: string) => void
+    sendMessageAC: () => void
+    dialogsPage: DialogsPageType
+}
