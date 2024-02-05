c7f811a518053c3940924a2931323169d7b6cac2
Sobicus <83043123+Sobicus@users.noreply.github.com>
2024-02-06 00:12:03 +0100

    SN #68 start

---
+++ samurai-way_68/src/components/common/preloader/Preloader.tsx
@@ -0,0 +1,6 @@
+import preloader from "../../../assets/img/preloader.gif";
+import React from "react";
+
+export const Preloader = () => {
+    return <div style={{backgroundColor: 'white'}}><img src={preloader} alt='preloader'/></div>
+}
