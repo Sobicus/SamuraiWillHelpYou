bd63ff7484296171dcade9ecc57bd15dea9990ed
Sobicus <83043123+Sobicus@users.noreply.github.com>
2024-02-12 17:55:02 +0100

    SCN #75 finish

--- samurai-way_75/src/components/Login/Login.tsx
+++ samurai-way_75/src/components/Login/Login.tsx
@@ -1,5 +1,86 @@
+import {Field, Formik, useFormik} from "formik";
+
 export const Login = () => {
-    return (
-        <div>LOGIN</div>
-    )
+    return <div>
+        <h1>Login</h1>
+        <Basic/>
+    </div>
+}
+const Basic = () => (
+    <div>
+        <Formik
+            initialValues={{
+                email: '',
+                password: '',
+                rememberMe: false
+            }}
+            validate={values => {
+                const errors: loginErrorType = {};
+                if (!values.email) {
+                    errors.email = 'Required';
+                } else if (
+                    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                ) {
+                    errors.email = 'Invalid email address';
+                }
+                if (!values.password) {
+                    errors.password = 'Required';
+                } else if (values.password.length < 4) {
+                    errors.password = 'Must be 4 characters or more';
+                }
+                return errors;
+            }}
+            onSubmit={(values, {setSubmitting}) => {
+                setTimeout(() => {
+                    alert(JSON.stringify(values, null, 2));
+                    setSubmitting(false);
+                }, 400);
+            }}
+        >
+            {({
+                  values,
+                  errors,
+                  touched,
+                  handleChange,
+                  handleBlur,
+                  handleSubmit,
+                  isSubmitting,
+                  /* and other goodies */
+              }) => (
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="email"
+                        name="email"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.email}
+                    />
+                    {errors.email && touched.email && errors.email}
+                    <input
+                        type="password"
+                        name="password"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.password}
+                    />
+                    {errors.password && touched.password && errors.password}
+                    <Field
+                        type="checkbox"
+                        name="rememberMe"
+                        // onChange={handleChange}
+                        // checked={values.rememberMe}
+                    />
+                    <button type="submit" disabled={isSubmitting}>
+                        Submit
+                    </button>
+                </form>
+            )}
+        </Formik>
+    </div>
+);
+
+type loginErrorType = {
+    email?: string
+    password?: string
+    rememberMe?: boolean
 }
